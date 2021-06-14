83d667bb1f323e29741abcb6ade717e96042182f
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2021-06-14 09:42:37 -0500

    Config de la coneccion a la base de datos mysql.

--- models/server.ts
+++ models/server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import userRoutes from "../routes/usuario";
-import cors from 'cors';
+import cors from "cors";
+import db from "../db/connection";
 class Server {
   private app: Application;
   private port: string;
@@ -8,15 +9,24 @@ class Server {
     usuarios: "/api/usuarios",
   };
   constructor() {
-    this.app = express();    
+    this.app = express();
     this.port = process.env.PORT || "5001";
+    this.dbConnection();
     this.middlewares();
     this.routes();
   }
+  async dbConnection() {
+    try {
+      await db.authenticate();
+      console.log("DB connected");
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
   middlewares() {
     this.app.use(cors()); // config default del cors
-    this.app.use(express.json()) // me parse el body en json
-    this.app.use(express.static('public'))//carpeta publica
+    this.app.use(express.json()); // me parse el body en json
+    this.app.use(express.static("public")); //carpeta publica
   }
   routes() {
     this.app.use(this.paths.usuarios, userRoutes);
